8ed8f0ace0f8364efaa1b2d08d36312c21fddab9
KryptomSilver <abelromeror763@gmail.com>
2021-10-11 11:41:28 -0500

    Format file

--- src/PageRender.tsx
+++ src/PageRender.tsx
@@ -4,27 +4,22 @@ import { NotFound } from "./components/NotFound";
 import { IParams } from "./interfaces/interfaces";
 
 const generatePage = (name: string) => {
-  const component = () => require(`./pages/${name}`).default
+  const component = () => require(`./pages/${name}`).default;
 
   try {
-    return React.createElement(component())
+    return React.createElement(component());
   } catch (err) {
     return <NotFound />;
   }
-}
+};
 
 const PageRender = () => {
-  const params = useParams()
-  console.log(params)
-  const { page, slug }: IParams = useParams()
-
-  let name = '';
-
-  if(page){
-    name = slug ? `${page}/[slug]` : `${page}`
+  const { page, slug }: IParams = useParams();
+  let name = "";
+  if (page) {
+    name = slug ? `${page}/[slug]` : `${page}`;
   }
+  return generatePage(name);
+};
 
-  return generatePage(name)
-}
-
-export default PageRender
+export default PageRender;
