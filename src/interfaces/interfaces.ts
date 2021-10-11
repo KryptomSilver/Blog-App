cb078100da87aea3f393cd05b3c20791327d74df
KryptomSilver <abelromeror763@gmail.com>
2021-10-11 11:40:55 -0500

    Added new interfaces

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,7 +1,31 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, FormEvent } from "react";
+import { AUTH } from "../redux/types";
+
+export type InputChange = ChangeEvent<HTMLInputElement>;
+export type FormSubmit = FormEvent<HTMLFormElement>;
 
 export interface IParams {
   page: string;
   slug: string;
 }
-export type InputChange = ChangeEvent<HTMLInputElement>;
+export interface IUserLogin {
+  account: string;
+  password: string;
+}
+export interface IUser extends IUserLogin {
+  _id: string;
+  avatar: string;
+  createdAt: string;
+  name: string;
+  role: string;
+  type: string;
+  updatedAt: string;
+}
+export interface IAuth {
+  token?: string;
+  user?: IUser;
+}
+export interface IAuthType {
+  type: typeof AUTH;
+  payload: IAuth;
+}
