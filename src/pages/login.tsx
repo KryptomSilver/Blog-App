44a4bd6a0f0d81d8c684b4660c24c89129ff7f88
KryptomSilver <abelromeror763@gmail.com>
2021-10-11 11:43:28 -0500

    Change view login and added ternary operator

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import LoginPass from "../components/auth/LoginPass";
+import LoginSMS from "../components/auth/LoginSMS";
 
 const Login = () => {
   const [sms, setSms] = useState(false);
@@ -8,16 +9,16 @@ const Login = () => {
     <div className="auth_page">
       <div className="auth_box">
         <h3 className="text-uppercase text-center mb-4">Login</h3>
-        <LoginPass />
+        {sms ? <LoginSMS /> : <LoginPass />}
         <small className="row my-2 text-primary" style={{ cursor: "pointer" }}>
           <span className="col-6">
             <Link to="/forgot_password" className="col-6 ">
               Forgot password
             </Link>
           </span>
-            <span className="col-6 text-end" onClick={() => setSms(!sms)}>
-              {sms ? "Sign in with password" : "Singn in with SMS"}
-            </span>
+          <span className="col-6 text-end" onClick={() => setSms(!sms)}>
+            {sms ? "Sign in with password" : "Singn in with SMS"}
+          </span>
         </small>
 
         <p>
