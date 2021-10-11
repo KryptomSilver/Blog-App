f712aa6483985a03ff952a8dd1b238cd0a2b000e
KryptomSilver <abelromeror763@gmail.com>
2021-10-11 15:04:43 -0500

    Added dispatch alert in to authActions

--- src/redux/actions/authActions.ts
+++ src/redux/actions/authActions.ts
@@ -1,21 +1,23 @@
 import { Dispatch } from "react";
 import { postAPI } from "../../helpers/FetchData";
-import {  IAuthType, IUserLogin } from "../../interfaces/interfaces";
-import { AUTH } from "../types";
+import { IAlertType, IAuthType, IUserLogin } from "../../interfaces/interfaces";
+import { ALERT, AUTH } from "../types";
 
 export const login =
-  (userLogin: IUserLogin) => async (dispatch: Dispatch<IAuthType>) => {
+  (userLogin: IUserLogin) =>
+  async (dispatch: Dispatch<IAuthType | IAlertType>) => {
     try {
+      dispatch({ type: ALERT, payload: { loading: true } });
       const res: any = await postAPI("login", userLogin);
       dispatch({
         type: AUTH,
         payload: {
           token: res.data.access_token,
           user: res.data.user,
-        }
+        },
       });
-      console.log(res.data)
+      dispatch({ type: ALERT, payload: { success: "Login Success!" } });
     } catch (error: any) {
-      console.log(error.response.data.msg);
+      dispatch({ type: ALERT, payload: { errors: error.response.data.msg } });
     }
   };
