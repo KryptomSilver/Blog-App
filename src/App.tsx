cfe56c47d0f4ac8acef43aa9d8b4b97eaf56dad0
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-14 15:43:17 -0500

    Added dependency from useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(refreshToken());
-  }, []);
+  }, [dispatch]);
   return (
     <div className="container">
       <Router>
