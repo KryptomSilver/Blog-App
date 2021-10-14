10b3800778c291dea42b5a1f6dd2d0398371a546
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-14 01:45:23 -0500

    Added on app refresh_token

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Alert } from "./components/alert/Alert";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import PageRender from "./PageRender";
+import { refreshToken } from "./redux/actions/authActions";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(refreshToken());
+  }, []);
   return (
     <div className="container">
       <Router>
