5b31d2111e5c645b2f2a8b74f165dbe8f82520cc
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-14 15:46:19 -0500

    Added action for login with google account.

--- src/redux/actions/authActions.ts
+++ src/redux/actions/authActions.ts
@@ -68,3 +68,18 @@ export const logout =
       dispatch({ type: ALERT, payload: { errors: error.response.data.msg } });
     }
   };
+export const googleLogin =
+  (id_token: string) => async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+    try {
+      dispatch({ type: ALERT, payload: { loading: true } });
+      const res: any = await postAPI("google_login", { id_token });
+      dispatch({
+        type: AUTH,
+        payload: res.data,
+      });
+      dispatch({ type: ALERT, payload: { success: res.data.msg } });
+      localStorage.setItem("logged", "true");
+    } catch (error: any) {
+      dispatch({ type: ALERT, payload: { errors: error.response.data.msg } });
+    }
+  };
