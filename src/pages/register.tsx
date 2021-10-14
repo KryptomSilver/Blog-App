e544a7681d93e0a6fc91d42b11cb09c6e47a27a1
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-13 23:25:36 -0500

    View for register user and styles.

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import RegisterForm from "../components/auth/RegisterForm";
 
 const Register = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="auth_page">
+      <div className="auth_box">
+        <h3 className="text-uppercase text-center mb-4">Register</h3>
 
-export default Register
+        <RegisterForm />
+        <p>
+          Already have an account?
+          <Link to="/login" style={{ color: "crimson", paddingLeft: ".4rem" }}>
+            Login Now
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
