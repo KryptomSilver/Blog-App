222b6060193e42a4a252777e88a6664d2c5d071e
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-18 02:00:18 -0500

    Added validation for password and confirmation.

--- src/helpers/Valid.ts
+++ src/helpers/Valid.ts
@@ -17,17 +17,19 @@ export const validRegister = ({
   } else if (!validateEmail(account) && !validatePhone(account)) {
     errors.push("Email or phone number format is incorrect.");
   }
-  if (!password) {
-    errors.push("Please add your password.");
-  } else if (password.length < 6) {
-    errors.push("Password must be at least 6 chars");
-  }else if (password!==cf_password){
-    errors.push("Confirm password did not match.")
-  }
+  const msg = checkPassword(password, cf_password);
+  if (msg) errors.push(msg);
   return {
     errMsg: errors,
     errLenght: errors.length,
   };
+};
+export const checkPassword = (password: string, cf_password: string) => {
+  if (password.length < 6) {
+    return "Password must be at least 6 chars";
+  } else if (password !== cf_password) {
+    return "Confirm password did not match.";
+  }
 };
 export const validatePhone = (phone: string) => {
   const re = /^[+]/g;
