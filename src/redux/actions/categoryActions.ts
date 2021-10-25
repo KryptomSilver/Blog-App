d81a23d0b61d47853c4b8abf96c27abddfc925ad
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-25 00:23:14 -0500

    Added actions delete and update category.

--- src/redux/actions/categoryActions.ts
+++ src/redux/actions/categoryActions.ts
@@ -1,10 +1,13 @@
 import { Dispatch } from "redux";
-import { getAPI, postAPI } from "../../helpers/FetchData";
+import { deleteAPI, getAPI, patchAPI, postAPI } from "../../helpers/FetchData";
+import { ICategory } from "../../interfaces/interfaces";
 import { ALERT, IAlertType } from "../types/alertType";
 import {
   CREATE_CATEGORY,
+  DELETE_CATEGORY,
   GET_CATEGORIES,
   ICategoryType,
+  UPDATE_CATEGORY,
 } from "../types/categoryType";
 
 export const createCategory =
@@ -16,7 +19,7 @@ export const createCategory =
       dispatch({ type: CREATE_CATEGORY, payload: res.data.newCategory });
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (error: any) {
-      dispatch({ type: ALERT, payload: error.response.data.msg });
+      dispatch({ type: ALERT, payload: { errors: error.response.data.msg } });
     }
   };
 
@@ -28,6 +31,32 @@ export const getCategories =
       dispatch({ type: GET_CATEGORIES, payload: res.data.categories });
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (error: any) {
-      dispatch({ type: ALERT, payload: error.response.data.msg });
+      dispatch({ type: ALERT, payload: { errors: error.response.data.msg } });
+    }
+  };
+export const updateCategory =
+  (data: ICategory, token: string) =>
+  async (dispatch: Dispatch<IAlertType | ICategoryType>) => {
+    try {
+      dispatch({ type: UPDATE_CATEGORY, payload: data });
+      const res: any = await patchAPI(
+        `category/${data._id}`,
+        { name: data.name },
+        token
+      );
+      dispatch({ type: ALERT, payload: { success: res.data.msg } });
+    } catch (error: any) {
+      dispatch({ type: ALERT, payload: { errors: error.response.data.msg } });
+    }
+  };
+export const deleteCategory =
+  (id: string, token: string) =>
+  async (dispatch: Dispatch<IAlertType | ICategoryType>) => {
+    try {
+      dispatch({ type: DELETE_CATEGORY, payload: id });
+      const res: any = await deleteAPI(`category/${id}`, token);
+      dispatch({ type: ALERT, payload: { success: res.data.msg } });
+    } catch (error: any) {
+      dispatch({ type: ALERT, payload: { errors: error.response.data.msg } });
     }
   };
