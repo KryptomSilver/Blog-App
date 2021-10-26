81e634c5053afd86c815b391507b8e1fb4dbc165
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-26 17:57:03 -0500

    Added handler for submit blog and image.

--- src/pages/create_blog.tsx
+++ src/pages/create_blog.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CardHoriz from "../components/cards/CardHoriz";
 import CreateForm from "../components/cards/CreateForm";
 import { NotFound } from "../components/NotFound";
-import { IBlog, RootStore } from "../interfaces/interfaces";
+import { FormSubmit, IBlog, RootStore } from "../interfaces/interfaces";
 import ReactQuill from "../components/editor/ReactQuill";
+import { validateBlog } from "../helpers/Valid";
+import { ALERT } from "../redux/types/alertType";
+import { imageUpload } from "../helpers/ImageUpload";
+import { createBlog } from "../redux/actions/blogActions";
 
 const CreateBlog = () => {
   const initialState = {
@@ -18,8 +22,26 @@ const CreateBlog = () => {
   };
   const [blog, setBlog] = useState<IBlog>(initialState);
   const [body, setBody] = useState("");
+  const [text, setText] = useState("");
+  const divRef = useRef<HTMLDivElement>(null);
   const { auth, categories } = useSelector((state: RootStore) => state);
   const dispatch = useDispatch();
+  useEffect(() => {
+    const div = divRef.current;
+    if (!div) return;
+    const text = div?.innerText as string;
+    setText(text);
+  }, [body]);
+  const handleSubmit = async () => {
+    if (!auth.access_token) return;
+    const check = validateBlog({ ...blog, content: text });
+    if (check.errLenght !== 0) {
+      return dispatch({ type: ALERT, payload: { errors: check.errMsg } });
+    }
+
+    const newData = { ...blog, content: body };
+    dispatch(createBlog(newData, auth.access_token));
+  };
   if (!auth.access_token) return <NotFound />;
   return (
     <div className="my-4 created_blog">
@@ -34,10 +56,19 @@ const CreateBlog = () => {
           <CardHoriz blog={blog} />
         </div>
       </div>
-        <ReactQuill setBody={setBody} />
-        <button className="btn btn-dark mt-3 d-block mx-auto">
-          Crete Post
-        </button>
+      <ReactQuill setBody={setBody} />
+      <div
+        ref={divRef}
+        dangerouslySetInnerHTML={{ __html: body }}
+        style={{ display: "none" }}
+      />
+      <small>{text.length}</small>
+      <button
+        className="btn btn-dark mt-3 d-block mx-auto"
+        onClick={handleSubmit}
+      >
+        Crete Post
+      </button>
     </div>
   );
 };
