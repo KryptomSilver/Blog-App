9e1afc32496cb8ee2cd214b1ee07019758198b07
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-28 11:16:44 -0500

    Added blogs in home view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import CardVert from "../components/cards/CardVert";
+import { IBlog, RootStore } from "../interfaces/interfaces";
 
 const Home = () => {
+  const { blogs } = useSelector((state: RootStore) => state);
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+    <div className="home_page">
+      {blogs.map((item) => (
+        <div key={item._id}>
+          {item.count > 0 && (
+            <>
+              <h3>
+                <Link to={`/blogs/${item.name}`}>
+                  {item.name}
+                  <small>({item.count})</small>
+                </Link>
+              </h3>
+              <hr className="mt-1" />
 
-export default Home
+              <div className="home_blogs">
+                {item.blogs.map((blog) => (
+                  <CardVert key={blog._id} blog={blog} />
+                ))}
+              </div>
+            </>
+          )}
+          {item.count > 4 && (
+            <Link
+              to={`/blogs/${item.name}`}
+              className="text-right d-block my-2 mb-3"
+            >
+              Read More &gt;&gt;
+            </Link>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
