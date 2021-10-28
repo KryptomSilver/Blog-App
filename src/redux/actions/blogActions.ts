2221a8927dff0bbc3f4a5ed302698ad0ccca6bc2
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-28 11:13:51 -0500

    Create action for getBlogs

--- src/redux/actions/blogActions.ts
+++ src/redux/actions/blogActions.ts
@@ -1,7 +1,9 @@
 import { Dispatch } from "redux";
+import { getAPI, postAPI } from "../../helpers/FetchData";
 import { imageUpload } from "../../helpers/ImageUpload";
 import { IBlog } from "../../interfaces/interfaces";
 import { ALERT, IAlertType } from "../types/alertType";
+import { GET_BLOGS, IGetBlogsType } from "../types/blogType";
 
 export const createBlog =
   (blog: IBlog, token: string) => async (dispatch: Dispatch<IAlertType>) => {
@@ -15,8 +17,19 @@ export const createBlog =
         url = blog.thumbnail;
       }
       const newBlog = { ...blog, thumbnail: url };
+      const res: any = await postAPI("blog", newBlog, token);
       dispatch({ type: ALERT, payload: { loading: false } });
     } catch (error: any) {
       dispatch({ type: ALERT, payload: { errors: error.response.data.msg } });
     }
   };
+export const getBlogs = () => async (dispatch: Dispatch<IAlertType | IGetBlogsType>) => {
+  try {
+    dispatch({ type: ALERT, payload: { loading: true } });
+    const res: any = await getAPI("blog");
+    dispatch({ type: GET_BLOGS, payload: res.data });
+    dispatch({ type: ALERT, payload: { loading: false } });
+  } catch (error: any) {
+    dispatch({ type: ALERT, payload: { errors: error.response.data.msg } });
+  }
+};
