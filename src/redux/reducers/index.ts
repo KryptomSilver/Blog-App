d96bd75faa5f3a52b18ef8936e67be7476216e59
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-28 11:15:14 -0500

    Added reducer in to index reducer

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -2,8 +2,11 @@ import { combineReducers } from "redux";
 import auth from "./authReducer";
 import alert from "./alertReducer";
 import categories from "./categoryReducer";
+import blogs from "./blogReducer";
+
 export default combineReducers({
   auth,
   alert,
   categories,
+  blogs,
 });
