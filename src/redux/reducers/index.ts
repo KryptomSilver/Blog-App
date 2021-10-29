c172921e6d0af8bf68739e282f82d03341b87e22
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-10-29 17:08:30 -0500

    Import reducer blogs by category

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -3,10 +3,12 @@ import auth from "./authReducer";
 import alert from "./alertReducer";
 import categories from "./categoryReducer";
 import blogs from "./blogReducer";
+import blogsCategory from "./blogCategoryReducer";
 
 export default combineReducers({
   auth,
   alert,
   categories,
   blogs,
+  blogsCategory
 });
