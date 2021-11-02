cafcf87ff2e327ba41677546f9266fcaddc96cb3
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-01 23:11:57 -0600

    Added interface and type for blogs by user id

--- src/redux/types/blogType.ts
+++ src/redux/types/blogType.ts
@@ -1,6 +1,7 @@
 import { IBlog } from "../../interfaces/interfaces";
 export const GET_BLOGS = "GET_BLOGS";
 export const GET_BLOGS_BY_CATEGORY = "GET_BLOGS_BY_CATEGORY";
+export const GET_BLOGS_BY_USER_ID = "GET_BLOGS_BY_USER_ID";
 
 export interface IGetBlogs {
   _id: string;
@@ -21,4 +22,14 @@ export interface IBlogsCategory {
 export interface IGetBlogsCategoryType {
   type: typeof GET_BLOGS_BY_CATEGORY;
   payload: IBlogsCategory;
+}
+export interface IBlogsUser {
+  id: string;
+  blogs: IBlog[];
+  total: number;
+  search: string;
+}
+export interface IGetBlogsUserType {
+  type: typeof GET_BLOGS_BY_USER_ID;
+  payload: IBlogsUser;
 }
