016f2152c5ed0b868bd5e6e4cd5d1d43a9a0c16e
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-08 11:25:37 -0600

    Component list comments

---
+++ src/components/comments/CommentList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { IComment } from "../../interfaces/interfaces";
+interface IProps {
+  comment: IComment;
+}
+const CommentList: React.FC<IProps> = ({ comment }) => {
+  return (
+    <div className="w-100">
+      <div className="comment_box">
+        <div
+          className="p-2"
+          dangerouslySetInnerHTML={{
+            __html: comment.content,
+          }}
+        />
+        <div className="d-flex justify-content-between p-2">
+          <small style={{ cursor: "pointer" }}>- Replay -</small>
+          <small>{new Date(comment.createAt).toLocaleString()}</small>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentList;
