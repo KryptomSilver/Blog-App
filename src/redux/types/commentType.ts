ceb68ce1dd32222d84c0d74d49c47e093b2e257c
KryptomSilver <32494744+KryptomSilver@users.noreply.github.com>
2021-11-11 12:34:53 -0600

    Types and interfaces comments

---
+++ src/redux/types/commentType.ts
@@ -0,0 +1,20 @@
+import { IComment } from "../../interfaces/interfaces";
+
+export const CREATE_COMMENT = "CREATE_COMMENT";
+export const GET_COMMENTS = "GET_COMMENTS";
+
+export interface ICommentState {
+  data: IComment[];
+  total: number;
+}
+
+export interface ICreateCommentType {
+  type: typeof CREATE_COMMENT;
+  payload: IComment;
+}
+export interface IGetCommentsType {
+  type: typeof GET_COMMENTS;
+  payload: ICommentState;
+}
+
+export type ICommentType = ICreateCommentType | IGetCommentsType;
